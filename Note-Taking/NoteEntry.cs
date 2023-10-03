44d6b7fba34c0c51aa7bf08c31dcf25fc9caa3e3
Sebastian Quintero <sebasq72@gmail.com>
2023-10-03 07:43:12 -0700

    Create model NoteEntry

---
+++ Note-Taking/NoteEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace Note_Taking.Data
+{
+    public class NoteEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+}
